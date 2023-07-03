f78e6d2b0ffa7ae08d25d91dd0f3257277170e9b
503133214 <503133214@qq.com>
2023-07-03 15:45:38 +0800

    更新了内容

--- code/C++/sort/sort.cpp
+++ code/C++/sort/sort.cpp
@@ -46,11 +46,43 @@ void selectSort(int arr[],int n){
         swap(arr[i],arr[min]);
     }
 }
+void merge(int arr[],int l,int m,int r){
+    int n1=m-l+1;
+    int n2=r-m;
+    int L[n1],R[n2];
+    for(int i=0;i<n1;i++){
+        L[i]=arr[l+i];
+    }
+    for(int i=0;i<n2;i++){
+        R[i]=arr[m+1+i];
+    }
+    int i=0,j=0,k=l;
+    while(i<n1&&j<n2){
+        if(L[i]<=R[j]){
+            arr[k]=L[i];
+            i++;
+        }else{
+            arr[k]=R[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        arr[k]=L[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        arr[k]=R[j];
+        j++;
+        k++;
+    }
+}
 int main(){
     int arr[10],n=10;
     randomArr(arr,n);
     printArr(arr,n);
-    bobuleSort(arr,n);
+    merge(arr,n, 0, n-1);
     printArr(arr,n);
     return 0;
 }
